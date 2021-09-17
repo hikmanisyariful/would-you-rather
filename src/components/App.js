239c4278e44e2e2c7aed304670fa6c1cd7af6a4d
hikmanisyariful <syarifulfajar017@gmail.com>
2021-09-17 14:07:59 +0700

    Fetch API - users and questions
    I try to fetch data users and question from available API.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import * as API from "../utils/api";
 
 class App extends Component {
+  state = {
+    users: [],
+    questions: []
+  };
+
+  componentDidMount() {
+    API.getInitialData().then(({ users, questions }) => {
+      this.setState(() => ({
+        users: users,
+        questions: questions
+      }));
+    });
+  }
+
   render() {
+    const { users, questions } = this.state;
     return (
       <div>
         <h1>Would You Rather?</h1>
+        <h2>USERS</h2>
+        {JSON.stringify(users)}
+
+        <h2>QUESTIONS</h2>
+        {JSON.stringify(questions)}
       </div>
     );
   }
