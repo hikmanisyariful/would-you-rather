464e4ef4fb16b5e19514496fa33d8e04d8fb173f
hikmanisyariful <syarifulfajar017@gmail.com>
2021-09-18 18:34:00 +0700

    feat: Adding Dropdown semantic ui
    To get value from Dropdown Component throught data
    
    onChange={this.handleChange}
    
    handleChange = (event, data) => {
      console.log(data.value);
      this.setState({ value: data.value });
    };

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,19 +5,21 @@ import { connect } from "react-redux";
 import { setAuthedUser } from "../actions/authedUser";
 import { Redirect } from "react-router-dom";
 
+import { Dropdown, Button, Header } from "semantic-ui-react";
+
 class Login extends Component {
   state = {
     value: "",
     toLogin: false
   };
 
-  handleChange = e => {
-    console.log(e.target.value);
-    this.setState({ value: e.target.value });
+  handleChange = (event, data) => {
+    console.log(data.value);
+    this.setState({ value: data.value });
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleSubmit = event => {
+    event.preventDefault();
     console.log("SUBMIT", this.state);
 
     if (this.state.value !== "") {
@@ -34,11 +36,20 @@ class Login extends Component {
     const { users } = this.props;
     const { toLogin } = this.state;
 
-    const userOptions = Object.keys(users).map(key => (
-      <option key={users[key].id} value={users[key].id}>
-        {users[key].name}
-      </option>
-    ));
+    const userOptions = [];
+
+    Object.keys(users).forEach(key => {
+      let userFormat = {
+        key: users[key].id,
+        text: users[key].name,
+        value: users[key].id,
+        image: {
+          avatar: true,
+          src: users[key].avatarURL
+        }
+      };
+      userOptions.push(userFormat);
+    });
 
     if (toLogin === true) {
       return <Redirect to="/questions" />;
@@ -47,18 +58,29 @@ class Login extends Component {
     return (
       <LoginContainer>
         <Title>
-          <h2>Welcome to the World You Rather App!</h2>
-          <h3>Please sign in to continue</h3>
+          <Header as="h2">Welcome to the World You Rather App!</Header>
+          <Header as="h3" disabled>
+            Please sign in to continue
+          </Header>
         </Title>
         <Logo>
           <img src={logo} alt="logo" />
         </Logo>
-        <FormSelect>
-          <h1>Sign In</h1>
-          <form onSubmit={this.handleSubmit}>
-            <Select onChange={this.handleChange}>{userOptions}</Select>
-            <Button type="submit">Submit</Button>
-          </form>
+        <FormSelect onSubmit={this.handleSubmit}>
+          <Header as="h1" color="teal">
+            Sign In
+          </Header>
+          <Dropdown
+            placeholder="Select User: "
+            fluid
+            selection
+            options={userOptions}
+            onChange={this.handleChange}
+          />
+          <br></br>
+          <Button fluid color="teal" type="submit">
+            Sign In
+          </Button>
         </FormSelect>
       </LoginContainer>
     );
@@ -69,6 +91,7 @@ const LoginContainer = styled.div`
   width: 700px;
   height: 500px;
   border: solid;
+  border-width: 2px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -77,11 +100,14 @@ const LoginContainer = styled.div`
 
 const Title = styled.div`
   width: 100%;
+  background: #dcdcdc;
   border-bottom-style: solid;
+  border-width: 2px;
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
+  padding: 20px 0;
 `;
 
 const Logo = styled.div`
@@ -90,24 +116,12 @@ const Logo = styled.div`
   padding: 10px;
 `;
 
-const FormSelect = styled.div`
-  width: 100%;
+const FormSelect = styled.form`
+  width: 80%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-const Select = styled.select`
-  min-width: 70%;
-  height: 40px;
-  margin-bottom: 10px;
-`;
-
-const Button = styled.button`
-  width: 70%;
-  height: 40px;
-  background: blue;
 `;
 
 function mapStateToProps({ users }) {
