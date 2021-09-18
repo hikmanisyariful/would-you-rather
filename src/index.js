30042b39d416f442b22e16656e8a71a060023d19
hikmanisyariful <syarifulfajar017@gmail.com>
2021-09-18 14:56:14 +0700

    fix: a bug when install semantic-ui for React
    Failed to compile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import "semantic-ui-less/semantic.less";
+
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
