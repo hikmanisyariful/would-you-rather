82be1d347417626f377ed4a11c73872bffc22ca9
hikmanisyariful <syarifulfajar017@gmail.com>
2021-09-18 18:33:18 +0700

    Install package - semantic-ui-react & semantic-ui-css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
+import "semantic-ui-css/semantic.min.css";
+
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
