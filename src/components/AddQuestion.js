f2d72b718b68423f82a6fdf4582bf323efa3fe79
hikmanisyariful <syarifulfajar017@gmail.com>
2021-09-19 16:53:33 +0700

    Handle React-Redux New Question

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
 import { Card, Form, Button, Header, Divider } from "semantic-ui-react";
 import styled from "styled-components";
+import { connect } from "react-redux";
+import { handleAddQuestion } from "../actions/questions";
+import { Redirect } from "react-router-dom";
 
 class AddQuestion extends Component {
+  state = {
+    optionOne: "",
+    optionTwo: "",
+    toHome: false
+  };
+
+  handleChange = e => {
+    this.setState(() => ({
+      [e.target.id]: e.target.value
+    }));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { optionOne, optionTwo } = this.state;
+    const { dispatch } = this.props;
+
+    dispatch(handleAddQuestion(optionOne, optionTwo));
+
+    this.setState(() => ({
+      optionOne: "",
+      optionTwo: "",
+      toHome: true
+    }));
+  };
+
   render() {
+    const { optionOne, optionTwo, toHome } = this.state;
+
+    if (toHome === true) {
+      return <Redirect to="/questions" />;
+    }
+
     return (
       <Container>
         <Card fluid>
@@ -13,13 +48,23 @@ class AddQuestion extends Component {
           <Card.Content extra>
             <p>Complete the question:</p>
             <Header as="h3">Would You Rather ...</Header>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Form.Field>
-                <input placeholder="Enter option one text here" />
+                <input
+                  placeholder="Enter option one text here"
+                  value={optionOne}
+                  id="optionOne"
+                  onChange={this.handleChange}
+                />
               </Form.Field>
               <Divider horizontal>OR</Divider>
               <Form.Field>
-                <input placeholder="Enter option two text here" />
+                <input
+                  placeholder="Enter option two text here"
+                  value={optionTwo}
+                  id="optionTwo"
+                  onChange={this.handleChange}
+                />
               </Form.Field>
               <Button fluid color="teal" type="submit">
                 Submit
@@ -36,4 +81,4 @@ const Container = styled.div`
   width: 500px;
 `;
 
-export default AddQuestion;
+export default connect()(AddQuestion);
