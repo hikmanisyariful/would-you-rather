e82d23b3b30d408a24819b0f1deaff05045317e1
hikmanisyariful <syarifulfajar017@gmail.com>
2021-09-19 14:45:59 +0700

    Make UI New Question

--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import { Card, Form, Button, Header, Divider } from "semantic-ui-react";
+import styled from "styled-components";
 
-const AddQuestion = () => {
-  return (
-    <div>
-      <h1>ADD QUESTION</h1>
-    </div>
-  );
-};
+class AddQuestion extends Component {
+  render() {
+    return (
+      <Container>
+        <Card fluid>
+          <Card.Content textAlign="center">
+            <Header as="h2">Create New Question</Header>
+          </Card.Content>
+          <Card.Content extra>
+            <p>Complete the question:</p>
+            <Header as="h3">Would You Rather ...</Header>
+            <Form>
+              <Form.Field>
+                <input placeholder="Enter option one text here" />
+              </Form.Field>
+              <Divider horizontal>OR</Divider>
+              <Form.Field>
+                <input placeholder="Enter option two text here" />
+              </Form.Field>
+              <Button fluid color="teal" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Card.Content>
+        </Card>
+      </Container>
+    );
+  }
+}
+
+const Container = styled.div`
+  width: 500px;
+`;
 
 export default AddQuestion;
